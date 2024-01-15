a3e45ee41b3e67c48469752e3e7752d18cc0688e
Ariel Roque <ariel.roque@hotmail.com>
2024-01-14 22:34:08 -0300

    feat: add initial structure

---
+++ cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+/media/ariel/Backup/Programacao/Projetos/Rust/toolbelt/cli/target/debug/cli: /media/ariel/Backup/Programacao/Projetos/Rust/toolbelt/cli/src/main.rs
---
+++ cli/target/debug/deps/cli-8d837de1d77e00e2.d
@@ -0,0 +1,5 @@
+/media/ariel/Backup/Programacao/Projetos/Rust/toolbelt/cli/target/debug/deps/cli-8d837de1d77e00e2: src/main.rs
+
+/media/ariel/Backup/Programacao/Projetos/Rust/toolbelt/cli/target/debug/deps/cli-8d837de1d77e00e2.d: src/main.rs
+
+src/main.rs:
